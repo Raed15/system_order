518676e922599c21cc2f3856b646d7ba065e6bde
YOUR NAME <YOUR EMAIL@SAMLECOM>
2023-02-23 02:50:34 +0300

    first commit

---
+++ myres.java
@@ -0,0 +1,16 @@
+
+
+public class myres {
+
+    public static void main(String[] args) {
+    
+   
+   item m2 = new item();
+   
+   System.out.println("-------------------------------------------------");
+
+
+    
+    }
+
+}
